cc1f3c5991607c0a84ed6cc5cb7af411ffa96b1d
Alex Xie <alexieyizhe@gmail.com>
2019-12-30 18:44:23 -0500

    Updates

---
+++ precache-manifest.6339f481bfc730dd5421c450b08d954b.js
@@ -0,0 +1,52 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/9d600a98d4f49f6cca6757b61f75bd3a.jpg"
+  },
+  {
+    "revision": "1821693f6594ff2fc68c85eecf3fc186",
+    "url": "/assets/cow-16x16.png"
+  },
+  {
+    "revision": "6e3863d42891947f64a3f67a8af1c989",
+    "url": "/assets/cow-32x32.png"
+  },
+  {
+    "revision": "9d600a98d4f49f6cca6757b61f75bd3a",
+    "url": "/assets/cow.jpg"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "d80f8608e38f1a27adf76a82298bb80f",
+    "url": "/assets/invisiblecow.jpg"
+  },
+  {
+    "revision": "8ad42028b334692a232f",
+    "url": "/bundle.8ad42.js"
+  },
+  {
+    "url": "/d80f8608e38f1a27adf76a82298bb80f.jpg"
+  },
+  {
+    "revision": "6e7493f16bce6d449c72e41c064ac493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abda0b3327d7f57c9c48",
+    "url": "/polyfills.abda0.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "d44e3e45905752cb110e75f07105e240",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "beec18519751ae79bfb6c12cb9a3246f",
+    "url": "/sw.js"
+  }
+]);
---
+++ precache-manifest.88c89e4356f228370feca270a1165977.esm.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/9d600a98d4f49f6cca6757b61f75bd3a.jpg"
+  },
+  {
+    "revision": "1821693f6594ff2fc68c85eecf3fc186",
+    "url": "/assets/cow-16x16.png"
+  },
+  {
+    "revision": "6e3863d42891947f64a3f67a8af1c989",
+    "url": "/assets/cow-32x32.png"
+  },
+  {
+    "revision": "9d600a98d4f49f6cca6757b61f75bd3a",
+    "url": "/assets/cow.jpg"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "d80f8608e38f1a27adf76a82298bb80f",
+    "url": "/assets/invisiblecow.jpg"
+  },
+  {
+    "revision": "f699c50a75c5e0e7b4276de99aa6f17e",
+    "url": "/bundle.6a1c0.esm.js"
+  },
+  {
+    "url": "/d80f8608e38f1a27adf76a82298bb80f.jpg"
+  },
+  {
+    "revision": "6e7493f16bce6d449c72e41c064ac493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e18bcb76466ff5fe18cd5163ce357f88",
+    "url": "/polyfills.d1bf4.esm.js"
+  }
+]);

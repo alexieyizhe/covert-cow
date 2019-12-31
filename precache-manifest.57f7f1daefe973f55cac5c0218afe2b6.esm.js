3bbb216382e8d10177482a01b25dd126e94ddab8
Alex Xie <alexieyizhe@gmail.com>
2019-12-30 20:06:00 -0500

    Updates

---
+++ precache-manifest.57f7f1daefe973f55cac5c0218afe2b6.esm.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/covert-cow9d600a98d4f49f6cca6757b61f75bd3a.jpg"
+  },
+  {
+    "revision": "1821693f6594ff2fc68c85eecf3fc186",
+    "url": "/covert-cowassets/cow-16x16.png"
+  },
+  {
+    "revision": "6e3863d42891947f64a3f67a8af1c989",
+    "url": "/covert-cowassets/cow-32x32.png"
+  },
+  {
+    "revision": "9d600a98d4f49f6cca6757b61f75bd3a",
+    "url": "/covert-cowassets/cow.jpg"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/covert-cowassets/icon.png"
+  },
+  {
+    "revision": "d80f8608e38f1a27adf76a82298bb80f",
+    "url": "/covert-cowassets/invisiblecow.jpg"
+  },
+  {
+    "revision": "ea21d678c84633f02776faa36025380b",
+    "url": "/covert-cowbundle.35d13.esm.js"
+  },
+  {
+    "url": "/covert-cowd80f8608e38f1a27adf76a82298bb80f.jpg"
+  },
+  {
+    "revision": "62d18ebdb41246efd45f30e33ccd53d3",
+    "url": "/covert-cowindex.html"
+  },
+  {
+    "revision": "70b9b27093e6dcabeed83a1bf554c735",
+    "url": "/covert-cowpolyfills.1bbb0.esm.js"
+  }
+]);

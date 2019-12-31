549d76e6a98ec8a74b101f923a3afe93c015c51c
Alex Xie <alexieyizhe@gmail.com>
2019-12-30 20:42:34 -0500

    🐛Fix public url serving from root instead of Github Pages subpath (#5)
    * Serve from relative github pages url
    
    * Fix url
    
    * Add public path to sound urls when sending request for buffer
    
    * Add analytics

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,16 @@
+const webpack = require('webpack');
+
 export default (config, env, _) => {
-  config.resolve.modules.push(env.src); // enable absolute imports
+  // enable absolute path imports in project
+  config.resolve.modules.push(env.src);
+
+  // serve from github pages url
+  const PUBLIC_PATH = env.production ? '/covert-cow/' : '';
+  config.output.publicPath = PUBLIC_PATH;
+  // use the public path in your app as 'process.env.PUBLIC_PATH'
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
+    })
+  );
 };

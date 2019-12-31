991cf021384d855cf8a9050e5d2378cf7a51ee9e
Alex Xie <alexieyizhe@gmail.com>
2019-12-30 20:15:04 -0500

    Fix url

--- preact.config.js
+++ preact.config.js
@@ -5,7 +5,7 @@ export default (config, env, _) => {
   config.resolve.modules.push(env.src);
 
   // serve from github pages url
-  const PUBLIC_PATH = env.production ? '/covert-cow' : '';
+  const PUBLIC_PATH = env.production ? '/covert-cow/' : '';
   config.output.publicPath = PUBLIC_PATH;
   // use the public path in your app as 'process.env.PUBLIC_PATH'
   config.plugins.push(
